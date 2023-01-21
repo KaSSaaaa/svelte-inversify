4feb82d77a240a7b813ff74baaa1349ab96438bc
Pierre Jacobs <pierre.jacobs852@gmail.com>
2023-01-21 20:40:48 +0100

    Fix typo

--- src/lib/components/ContainerContext.svelte
+++ src/lib/components/ContainerContext.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Container } from 'inversify';
-	import { setContainer } from '../functions/svelte-inversify';
+	import { setContainer } from '../functions/functions';
 
 	export let container: Container;
 
